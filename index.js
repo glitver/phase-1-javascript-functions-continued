99e53d7c3d77e100b9d751bcd790322e7a28d59a
+ <+>
2024-03-17 21:23:33 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(visualFlair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+}
+const encouragingPromptFunction = wrapAdjective('*');
+console.log(encouragingPromptFunction('incredible'));
+const encouragingBarsFunction = wrapAdjective('||');
+console.log(encouragingBarsFunction('awesome'));
